2d5fe335f1a1fe272018cb079f06ac2608d8b112
Jann1s <luegering.jannis@googlemail.com>
2019-03-05 12:23:34 +0100

    - Filled the IThreading interface - Tested a bit the Tasks

--- ThreadRacer/Threading/IThreading.cs
+++ ThreadRacer/Threading/IThreading.cs
@@ -8,5 +8,6 @@ namespace ThreadRacer.Threading
 {
     interface IThreading
     {
+        float Start();
     }
 }
--- ThreadRacer/Threading/Task.cs
+++ ThreadRacer/Threading/Task.cs
@@ -3,10 +3,63 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThreadRacer.Tracks;
 
 namespace ThreadRacer.Threading
 {
     class Task
     {
+        public float Start(ITrack track)
+        {
+            int maxThreads = 5;
+            List<System.Threading.Tasks.Task> tasks = new List<System.Threading.Tasks.Task>();
+
+            List<Func<int>> methods = new List<Func<int>>();
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+            methods.Add(TestTask);
+
+            int index = 0;
+            int methodCount = methods.Count / maxThreads;
+            int buffer = methods.Count % maxThreads;
+
+            for (int i = 0; i < maxThreads; i++)
+            {
+                System.Threading.Tasks.Task task = new System.Threading.Tasks.Task(() =>
+                {
+                    for (int j = 0; j < methodCount; j++)
+                    {
+                        methods[index]();
+                        index++;
+                    }
+                });
+
+                tasks.Add(task);
+            }
+
+            foreach(System.Threading.Tasks.Task t in tasks)
+            {
+                t.Start();
+            }
+
+            return 0;
+        }
+
+        private int TestTask()
+        {
+            for (int i = 0; i < 10000000; i++)
+            {
+                float test = (156 / 5) * i;
+            }
+
+            return 0;
+        }
     }
 }
