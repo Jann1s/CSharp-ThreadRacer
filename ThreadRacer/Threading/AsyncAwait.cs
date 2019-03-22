27fe59d0882e5d1294de039cff491734ae08f95f
vadim295 <vadim.derevicev@web.de>
2019-03-22 12:45:11 +0100

    Basic layout changes

--- ThreadRacer/Threading/AsyncAwait.cs
+++ ThreadRacer/Threading/AsyncAwait.cs
@@ -8,6 +8,21 @@ namespace ThreadRacer.Threading
 {
     class AsyncAwait : IThreading
     {
+        public bool Start(ITrack track, int numberOfThreads)
+        {
+            List<Func<bool>> functions = track.GetFunctions();
 
+            for (int i = 0; i < functions.Count; i++)
+            {
+                    // custom implementation here 
+            }
+
+            //wait for the tasks to complete and then return true
+
+           
+     
+
+            return true;
+        }
     }
 }
