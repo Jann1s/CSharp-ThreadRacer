54d5afd06e7ba6d2786d3751a41e9e37f47d39d1
me <me@DESKTOP-G12I2BQ>
2019-03-27 11:16:41 +0100

    Linq

--- ThreadRacer/Threading/AsyncAwait.cs
+++ ThreadRacer/Threading/AsyncAwait.cs
@@ -3,11 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ThreadRacer.Tracks;
 
 namespace ThreadRacer.Threading
 {
     class AsyncAwait : IThreading
     {
-
+        public bool Start(ITrack track, int numberOfThreads)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
--- ThreadRacer/Threading/LINQMethod.cs
+++ ThreadRacer/Threading/LINQMethod.cs
@@ -2,12 +2,53 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Thread = System.Threading.Tasks;
+using ThreadRacer.Tracks;
 
 namespace ThreadRacer.Threading
 {
     class LINQMethod : IThreading
     {
+        public bool Start(ITrack track, int numberOfThreads)
+        {
+            List<Thread.Task> tasks = new List<Thread.Task>();
+            List<Func<bool>> functions = track.GetFunctions();
 
+            int index = 0;
+            int methodCount = functions.Count / numberOfThreads;
+            int buffer = functions.Count % numberOfThreads;
+
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                if (index < functions.Count)
+                {
+                    Thread.Task task = new Thread.Task(() =>
+                    {
+                        functions[index]();
+                        index++;
+
+                        if (buffer >= 1)
+                        {
+                            functions[index]();
+                            index++;
+                        }
+                    });
+
+                    tasks.Add(task);
+                }
+            }
+
+            IEnumerable<Thread.Task> linqQuery = from task in tasks.AsParallel()
+                                                 where task != null
+                                                 select task;
+            foreach (Thread.Task item in linqQuery)
+            {
+                item.Start();
+            }
+
+            Thread.Task.WaitAll(tasks.ToArray());
+
+            return true;
+        }
     }
 }
