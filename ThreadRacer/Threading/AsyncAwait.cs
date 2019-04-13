147438e73c1e9beac4e5c2c07fceccf9cc83e946
vadim295 <vadim.derevicev@web.de>
2019-04-13 23:25:17 +0200

    Nothing special

--- ThreadRacer/Threading/AsyncAwait.cs
+++ ThreadRacer/Threading/AsyncAwait.cs
@@ -2,27 +2,69 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using Thread = System.Threading.Tasks;
+using ThreadRacer.Tracks;
 
 namespace ThreadRacer.Threading
 {
-    //class AsyncAwait : IThreading
-    //{
-    //    public bool Start(ITrack track, int numberOfThreads)
-    //    {
-    //        List<Func<bool>> functions = track.GetFunctions();
+    class AsyncAwait : IThreading
+    {
+        public async bool Start(ITrack track, int numberOfThreads)
+        {
+            List<Thread.Task> tasks = new List<Thread.Task>();
+            List<Func<bool>> functions = track.GetFunctions();
 
-    //        for (int i = 0; i < functions.Count; i++)
-    //        {
-    //                // custom implementation here 
-    //        }
+            int index = 0;
+            int methodCount = functions.Count / numberOfThreads;
+            int buffer = functions.Count % numberOfThreads;
 
-    //        //wait for the tasks to complete and then return true
+            for (int i = 0; i < numberOfThreads; i++)
+            {
+                if (index < functions.Count)
+                {
+                    Thread.Task task = new Thread.Task(() =>
+                    {
+                        functions[index]();
+                        index++;
 
-           
-     
+                        if (buffer >= 1)
+                        {
+                            await functions[index]();
+                            index++;
+                        }
+                    });
 
-    //        return true;
-    //    }
-    //}
+                    tasks.Add(task);
+                }
+            }
+
+            foreach (Thread.Task t in tasks)
+            {
+                t.Start();
+            }
+
+            Thread.Task.WaitAll(tasks.ToArray());
+
+            return true;
+        }
+
+
+
+
+
+
+        //public bool Start(ITrack track, int numberOfThreads)
+        //{
+        //    List<Func<bool>> functions = track.GetFunctions();
+
+        //    for (int i = 0; i < functions.Count; i++)
+        //    {
+        //        // custom implementation here 
+        //    }
+
+        //    //wait for the tasks to complete and then return true
+
+        //    return true;
+        //}
+    }
 }
