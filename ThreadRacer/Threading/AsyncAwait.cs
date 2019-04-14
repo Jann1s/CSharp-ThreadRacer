e0da39c60f4400443408250d3ca01c7bd8ee75bf
vadim295 <vadim.derevicev@web.de>
2019-04-14 17:13:35 +0200

    Done
    AsyncAwait done

--- ThreadRacer/Threading/AsyncAwait.cs
+++ ThreadRacer/Threading/AsyncAwait.cs
@@ -2,70 +2,39 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Thread = System.Threading.Tasks;
-using System.Threading.Tasks;
+using microTasks = System.Threading.Tasks;
 using ThreadRacer.Tracks;
 
 namespace ThreadRacer.Threading
 {
     class AsyncAwait : IThreading
     {
-        public async bool Start(ITrack track, int numberOfThreads)
+        public bool Start(ITrack track, int numberOfThreads)
         {
-            List<Thread.Task> tasks = new List<Thread.Task>();
-            List<Func<bool>> functions = track.GetFunctions();
-
-            int index = 0;
-            int methodCount = functions.Count / numberOfThreads;
-            int buffer = functions.Count % numberOfThreads;
-
-            for (int i = 0; i < numberOfThreads; i++)
-            {
-                if (index < functions.Count)
-                {
-                    Thread.Task task = new Thread.Task(() =>
-                    {
-                        functions[index]();
-                        index++;
-
-                        if (buffer >= 1)
-                        {
-                            await functions[index]();
-                            index++;
-                        }
-                    });
-
-                    tasks.Add(task);
-                }
-            }
-
-            foreach (Thread.Task t in tasks)
-            {
-                await t.Start();
-            }
-
-            Thread.Task.WaitAll(tasks.ToArray());
-
+            AsyncTask(track, numberOfThreads);
             return true;
         }
 
+        public async void AsyncTask(ITrack track, int numberOfThreads)
+        {
+            List<microTasks.Task> tasks = new List<microTasks.Task>();
+            List<Func<bool>> functions = track.GetFunctions();
 
+            foreach (Func<bool> func in functions)
+            {
+                microTasks.Task task = new microTasks.Task(() =>
+                {
+                    func();
+                });
 
+                tasks.Add(task);
+            }
 
-
-
-        //public bool Start(ITrack track, int numberOfThreads)
-        //{
-        //    List<Func<bool>> functions = track.GetFunctions();
-
-        //    for (int i = 0; i < functions.Count; i++)
-        //    {
-        //        // custom implementation here 
-        //    }
-
-        //    //wait for the tasks to complete and then return true
-
-        //    return true;
-        //}
+            foreach (microTasks.Task t in tasks)
+            {
+                t.Start();
+                await t;
+            }
+        }
     }
 }
