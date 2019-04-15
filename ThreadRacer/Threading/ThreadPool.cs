75cb2539bfddc3705264e6c59846e04f46d626ae
elvinasl <elvinas.lukasevicius@gmail.com>
2019-04-15 11:01:37 +0200

    Threadpool fixed

--- ThreadRacer/Threading/ThreadPool.cs
+++ ThreadRacer/Threading/ThreadPool.cs
@@ -5,38 +5,66 @@ using System.Text;
 using System.Threading.Tasks;
 using ThreadRacer.Tracks;
 using Windows.Foundation;
+using Windows.System.Threading;
 using TP = Windows.System.Threading;
 
 namespace ThreadRacer.Threading
 {
     class ThreadPool : IThreading
     {
+        int totalCompleted;
+        int needToComplete;
+
         public bool Start(ITrack track, int numberOfThreads)
         {
             List<Func<bool>> functions = track.GetFunctions();
+            needToComplete = functions.Count;
 
-            uint totalFunctionsCompleted = 0;
-
-            foreach(Func<bool> func in functions)
+            foreach (Func<bool> func in functions)
             {
-                IAsyncAction asyncAction = TP.ThreadPool.RunAsync((workItem) =>
+                // dump all the work to the threadpool
+                IAsyncAction asyncAction = TP.ThreadPool.RunAsync(
+                (workItem) =>
                 {
                     func();
                 });
 
-                // task is complete
-                asyncAction.Completed = new AsyncActionCompletedHandler((IAsyncAction asyncInfo, AsyncStatus asyncStatus) =>
-                {
-                    totalFunctionsCompleted++;
-                });
+                // tell threadpool what happens when the task is completed
+                asyncAction.Completed = new AsyncActionCompletedHandler(
+                       (IAsyncAction asyncInfo, AsyncStatus asyncStatus) =>
+                       {
+                           if (asyncStatus == AsyncStatus.Completed)
+                           {
+                               totalCompleted++;
+                           }
+                       });
             }
 
-            if(functions.Count == totalFunctionsCompleted)
+            return isAllCompleted();
+        }
+
+        // check if all the tasks are completed
+        private bool isAllCompleted()
+        {
+            bool isAllDone = false;
+
+                while (isAllDone == false)
+                {
+                    if(totalCompleted == needToComplete)
+                    {
+                        isAllDone = true;
+                    }
+                } 
+
+            if(isAllDone)
             {
                 return true;
+            } else
+            {
+                return false;
             }
-
-            return false;
         }
+
+
     }
 }
